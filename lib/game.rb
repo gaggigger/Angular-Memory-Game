079712210f672a7ce26b9eaadfa422049028f031
trivektor <trivektor@gmail.com>
2013-07-07 14:37:00 -0700

    return set of cards to be displayed on game stage

--- lib/game.rb
+++ lib/game.rb
@@ -6,6 +6,9 @@ class Game
     selected_cards = ICONS.take(18)
     selected_cards *= 2
     selected_cards.shuffle
+    [].tap do |results|
+      selected_cards.each_with_index { |item, index| results << {index: index, icon: item} }
+    end
   end
 
 end
