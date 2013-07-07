8a34881a4a2be552a3e46b601d9865744ccefb25
trivektor <trivektor@gmail.com>
2013-07-07 14:37:20 -0700

    add logic to flip cards

--- public/javascripts/game_controller.js
+++ public/javascripts/game_controller.js
@@ -1,9 +1,23 @@
 app.controller('GameController', function($scope, gameService) {
 
   $scope.cards = gameService.cards;
+  $scope.flippedCards = [];
+
+  var MAX_SHOWN = 2;
 
   $scope.startGame = function() {
     gameService.fetchCards();
+  }
+
+  $scope.flip = function(card) {
+    if ($scope.flippedCards.length === MAX_SHOWN) {
+      $scope.flippedCards.shift();
+    }
+    $scope.flippedCards.push(card.index);
+  }
+
+  $scope.isShown = function(card) {
+    return $scope.flippedCards.indexOf(card.index) !== -1
   }
 
 })
