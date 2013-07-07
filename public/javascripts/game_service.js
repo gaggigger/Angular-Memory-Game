340625d06d470fe1dabdc5433c8400d5a41395cf
trivektor <trivektor@gmail.com>
2013-07-07 15:38:37 -0700

    change syntax of ajax request

--- public/javascripts/game_service.js
+++ public/javascripts/game_service.js
@@ -2,13 +2,14 @@ app.factory('gameService', ['$http', function($http) {
   var cards = [];
 
   function fetchCards() {
-    $http({
-      url: '/game/cards',
-      method: 'GET'
-    })
-    .success(function(data, status, headers, config) {
-      angular.copy(data.cards, cards);
-    })
+    $http
+      .get('/game/cards')
+      .then(function (response) {
+        /* 'response' is of the format
+         * {config: [Object object], data: [Object object], status: [Object object], headers: [Object object]}
+         */
+        angular.copy(response.data.cards, cards)
+      });
   }
 
   return {
