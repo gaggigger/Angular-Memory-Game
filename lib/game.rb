67781b9f719fca3faffaaa928bb742ac8db4fe89
trivektor <trivektor@gmail.com>
2013-07-07 18:46:35 -0700

    update logic to randomize cards

--- lib/game.rb
+++ lib/game.rb
@@ -3,7 +3,7 @@ class Game
   include FontAwesome
 
   def self.fetch_cards
-    selected_cards = ICONS.take(18)
+    selected_cards = ICONS.sample(18)
     selected_cards *= 2
     selected_cards.shuffle
     [].tap do |results|
